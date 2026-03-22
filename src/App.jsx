22e5ed7e0983cd4ea6dd7ef98fe808ee10bf7257
Morshedul-developer <nijhumkhandoker9@gmail.com>
2026-03-22 21:15:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <Para></Para>
+        </div>
+        </section>
+    </>
+  )
+}
+
+function Para() {
+  return(
+    <p>Hello Nice</p>
+  )
+}
+
+export default App
